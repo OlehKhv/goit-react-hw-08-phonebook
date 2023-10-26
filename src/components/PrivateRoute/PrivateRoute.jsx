49fd6b0038f0fbfccac013f3dbbd995fdff51a52
OlehKhv <126176715+OlehKhv@users.noreply.github.com>
2023-10-26 22:25:02 +0300

    fix navigate

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,11 +1,16 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { selectLoggedIn, selectRefreshing } from 'redux/auth/selectors';
 
 export const PrivateRoute = ({ component: Component }) => {
     const isLoggedIn = useSelector(selectLoggedIn);
     const isRefreshing = useSelector(selectRefreshing);
     const shouldRedirect = !isLoggedIn && !isRefreshing;
+    const location = useLocation();
 
-    return shouldRedirect ? <Navigate to="/login" /> : <Component />;
+    return shouldRedirect ? (
+        <Navigate to="/login" state={location} />
+    ) : (
+        <Component />
+    );
 };
--- src/components/RestrictedRoute/RestrictedRoute.jsx
+++ src/components/RestrictedRoute/RestrictedRoute.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { selectLoggedIn } from 'redux/auth/selectors';
 
 export const RestrictedRoute = ({ component: Component }) => {
     const isLoggedIn = useSelector(selectLoggedIn);
 
-    return isLoggedIn ? <Navigate to="/" /> : <Component />;
+    const { state } = useLocation();
+
+    return isLoggedIn ? <Navigate to={state ? state : '/'} /> : <Component />;
 };
